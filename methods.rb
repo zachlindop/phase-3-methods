d8c000091defdf2a500626e340502162526cfa61
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-08-29 16:02:41 -0700

    commit tree 90ef3ef38d7b630642ad09c8965abc210974c144

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default "Naureen"
+
+def add(num1, num2)
+    return num1 + num2
+end
+add(2,5)
+
+def halve(num1)
+    return num1/2
+end
+halve(6)
+
+#def halve (num1)3
+#    if name.class != integer
+#        return nil
+#    end
+
+#    num1.reverse/2
+#end
+#halve(6)
